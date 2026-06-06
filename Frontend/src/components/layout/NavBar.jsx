c21f2193288fa60d35baede92abe4a0a0ae9a088
Raj Singh <raj.kr.singh.er@gmail.com>
2026-06-07 00:09:51 +0545

    feat: restructure frontend assets and components
    - Removed unused react.svg and vite.svg files.
    - Added new Spotify logo assets in various formats (JPEG, PNG, SVG, WEBP).
    - Created NavBar and TextShow components for navigation.
    - Implemented HomePage with NavBar integration.
    - Added placeholder pages for AlbumView, LoginPage, SignupPage, and StudioView.
    - Updated index.css to include Tailwind CSS.
    - Configured Vite to use Tailwind CSS plugin.
    - Initialized package.json and package-lock.json for project dependencies.

---
+++ Frontend/src/components/layout/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TextShow } from '../shared/TextShow'
+
+const NavBar = () => {
+  return (
+    <div>
+      <TextShow text='Premium'/>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ Frontend/src/components/shared/TextShow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TextShow = (props) => {
+  return (
+    <div className=' absolute font-bold text-gray-500  hover:scale-105 hover:text-white' >
+        {props.text}
+
+    </div>
+  )
+}
+ 
